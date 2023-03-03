4a5724d6bdf0fdd9c617044f55465a36070bfbfa
Artem Zapara <art.zapara@gmail.com>
2023-03-03 19:52:35 +0200

    Changed appearance using Rich

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-from utils import get_secter_word
+from utils import get_secret_word, show_guesses, game_over, refresh_page
+from rich.console import Console
+from rich.theme import Theme
+
+console = Console(width=40, theme=Theme({"warning": "red on yellow"}))
 
 def main():
-    secret_word = get_secter_word()
-    for guess_num in range(1,7):
-        guess = input("Guess the word: ").upper()
-        if guess == secret_word:
-            print("Correct")
+    secret_word = get_secret_word()
+    guesses = ["_" * 5] * 6
+
+    for idx in range(6):
+        headline = f"Guess {idx + 1}"
+        refresh_page(headline, console)
+        show_guesses(guesses, secret_word, console)
+
+        guesses[idx] = input(f"Guess word: ").upper()
+        if guesses[idx] == secret_word:
             break
 
-        correct_letters = {letter for letter, correct in zip(guess, secret_word) if letter == correct}
-        misplaced_letters = set(guess) & set(secret_word) - correct_letters
-        wrong_letters = set(guess) - set(secret_word)
-
-        print("Correct letters:", ", ".join(correct_letters))
-        print("Misplaced letters:", ", ".join(misplaced_letters))
-        print("Wrong letters:", ", ".join(wrong_letters))
-
-    else:
-        print(f"The secter word was {secret_word}")
+    game_over(guesses, secret_word, console, guessed_correctly=guesses[idx] == secret_word)
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 import random
 from string import ascii_letters
 
-def get_secter_word():
+def get_secret_word():
     wordList = []
     with open("wordList.txt") as f:
         lines = f.readlines()
@@ -12,3 +12,33 @@ def get_secter_word():
                 wordList.append(word)
 
     return random.choice(wordList)
+
+def show_guesses(guesses, secret_word, console):
+    for guess in guesses:
+        styled_guess = []
+        for letter, correct in zip(guess, secret_word):
+            if letter == correct:
+                style = "bold white on green"
+            elif letter in secret_word:
+                style = "bold white on yellow"
+            elif letter in ascii_letters:
+                style = "white on #666666"
+            else:
+                style = "dim"
+            styled_guess.append(f"[{style}]{letter}[/]")
+
+        console.print("".join(styled_guess), justify="center")
+
+def refresh_page(headline, console):
+    console.clear()
+    console.rule(f"[bold blue]:leafy_green: {headline} :leafy_green:[/]\n")
+
+def game_over(guesses, word, console, guessed_correctly):
+    headline = "Game Over"
+    refresh_page(headline, console)
+    show_guesses(guesses, word, console)
+
+    if guessed_correctly:
+        console.print(f"\n[bold white on green]Correct, the secret word is {word}[/]")
+    else:
+        console.print(f"\n[bold white on red]The secret word was {word}[/]")
